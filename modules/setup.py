a955a27ebd7bc197580f719564ba5346fe6f01b0
Fahri Reza <dozymoe@gmail.com>
2017-01-25 22:30:21 +0700

    module.setup create virtualenv and init project

---
+++ modules/setup.py
@@ -0,0 +1,57 @@
+"""Waf setup virtualenv and project."""
+import os
+from shutil import rmtree
+from subprocess import check_call
+import sys
+
+def setup(loader):
+    """Setup virtualenv and project."""
+
+    work_dir = loader.config['work_dir']
+    python_version = loader.config['python_version']
+
+    venv_dir = os.path.realpath(os.path.join(work_dir,
+            loader.config['virtualenv_dir']))
+
+    if os.path.exists(venv_dir):
+        rmtree(venv_dir)
+
+    check_call(['virtualenv', '--python=python' + python_version, venv_dir])
+
+    modules_link = os.path.realpath(os.path.join(work_dir, 'python_modules'))
+
+    if os.path.exists(modules_link):
+        os.remove(modules_link)
+
+    os.symlink(os.path.join(venv_dir, 'lib', 'python' + python_version,
+            'site-packages'), modules_link)
+
+    os.environ['PATH'] = os.path.join(venv_dir, 'bin') + ':' +\
+            os.environ['PATH']
+
+    sys.path.insert(1, modules_link)
+
+    check_call(['pip', 'install', '--upgrade', 'pip'])
+
+    if os.path.exists(os.path.join(work_dir, 'requirements.txt')):
+        check_call(['pip', 'install', '-r', os.path.join(work_dir,
+                'requirements.txt')])
+
+    if os.path.exists(os.path.join(work_dir, '.git')):
+        check_call(['git', 'submodule', 'update', '--init', '--recursive'])
+
+    if os.path.exists(os.path.join(work_dir, 'package.json')):
+        check_call(['npm', 'install'])
+
+    if os.path.exists(os.path.join(work_dir, 'bower.json')):
+        check_call(['bower', 'install'])
+
+    if os.path.exists(os.path.join(work_dir, 'wscript')):
+        check_call(['waf', 'configure'])
+        check_call(['waf', 'clean', 'build'])
+
+    if os.path.exists(os.path.join(work_dir, 'alembic.ini')):
+        check_call(['alembic', 'upgrade', 'head'])
+
+
+commands = (setup,)
