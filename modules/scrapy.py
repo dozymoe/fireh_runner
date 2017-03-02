3e1c38dbcda184a4d15c21e911d48a0915d843a6
Fahri Reza <dozymoe@gmail.com>
2017-03-02 23:05:28 +0700

    new, module scrapy.py

---
+++ modules/scrapy.py
@@ -0,0 +1,36 @@
+"""Scrapy module.
+
+An open source and collaborative framework for extracting the data you need
+from websites. In a fast, simple, yet extensible way.
+"""
+import os
+
+def scrapy(loader, project=None, variant=None, *args):
+    """Extract data from websites."""
+    loader.setup_virtualenv()
+
+    project, variant = loader.setup_project_env(project, variant)
+
+    config = loader.config.get('configuration', {})
+    config = config.get(variant, {})
+    config = config.get(project, {})
+
+    loader.setup_shell_env(config.get('shell_env', {}))
+
+    work_dir = config.get('work_dir', project)
+    work_dir = os.path.join(loader.config['work_dir'], work_dir)
+
+    binargs = ['scrapy'] + list(args)
+    os.chdir(work_dir)
+    os.execvp(binargs[0], binargs)
+
+
+def scrapy_startproject(loader, *args):
+    """Create new scrapy project."""
+    loader.setup_virtualenv()
+
+    binargs = ['scrapy', 'startproject'] + list(args)
+    os.execvp(binargs[0], binargs)
+
+
+commands = (scrapy, scrapy_startproject)
