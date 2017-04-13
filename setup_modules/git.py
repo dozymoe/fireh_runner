189dfec84f1922d419f5f48629f9f8429deae841
Fahri Reza <dozymoe@gmail.com>
2017-04-13 13:40:51 +0700

    git pull before git submodule update

--- setup_modules/git.py
+++ setup_modules/git.py
@@ -5,4 +5,5 @@ def setup(loader, variant):
     work_dir = loader.config['work_dir']
 
     if os.path.exists(os.path.join(work_dir, '.git')):
+        check_call(['git', 'pull'])
         check_call(['git', 'submodule', 'update', '--init', '--recursive'])
