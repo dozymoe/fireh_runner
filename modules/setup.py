f81a85fc5e10e7cd1507a2c328dae473e68aaa99
Fahri Reza <dozymoe@gmail.com>
2017-04-14 10:22:16 +0700

    change current dir before each modules

--- modules/setup.py
+++ modules/setup.py
@@ -9,6 +9,7 @@ def setup(loader, variant=None):
                 loader.config['default_variant'])
 
     for module_name in loader.config.get('setup_modules', []):
+        os.chdir(loader.config['work_dir'])
         mod = loader.load_module(module_name)
         try:
             func = getattr(mod, 'setup')
