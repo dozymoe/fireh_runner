fdc3816c5b44cbf9bda2bcd4d78233e7e4edd6ef
Fahri Reza <dozymoe@gmail.com>
2018-05-27 13:41:36 +0700

    new commands: odoo-install, odoo-uninstall, odoo-upgrade

--- modules/odoo.py
+++ modules/odoo.py
@@ -5,6 +5,8 @@ Odoo is an OpenERP framework.
 Website: http://www.odoo.com
 """
 import os
+import pexpect
+import sys
 
 
 def odoo(loader, project=None, variant=None, *args):
@@ -36,6 +38,209 @@ def odoo(loader, project=None, variant=None, *args):
     os.execvp(binargs[0], binargs)
 
 
+def odoo_shell(loader, project=None, variant=None, *args):
+    loader.setup_virtualenv()
+
+    project, variant = loader.setup_project_env(project, variant)
+
+    config = loader.config.get('configuration', {})
+    config = config.get(variant, {})
+    config = config.get(project, {})
+
+    config_file = config.get('config_file')
+    config_file = os.path.join(loader.config['work_dir'],
+            config_file)
+
+    loader.setup_shell_env(config.get('shell_env', {}))
+
+    work_dir = config.get('work_dir', project)
+    work_dir = loader.expand_path(work_dir)
+
+    python_bin = loader.get_python_bin()
+    # bugfix, command like `odoo.py shell`, the word 'shell'  must be mentioned
+    # before we define --config, weird
+    binargs = [python_bin, __file__]
+    binargs.append('shell')
+    binargs.append('--no-xmlrpc')
+    binargs += list(args)
+    if config_file:
+        binargs.append('--config=' + config_file)
+
+    os.chdir(work_dir)
+    os.execvp(binargs[0], binargs)
+
+
+def odoo_install(loader, project=None, variant=None, *args):
+    loader.setup_virtualenv()
+
+    project, variant = loader.setup_project_env(project, variant)
+
+    config = loader.config.get('configuration', {})
+    config = config.get(variant, {})
+    config = config.get(project, {})
+
+    config_file = config.get('config_file')
+    config_file = os.path.join(loader.config['work_dir'],
+            config_file)
+
+    loader.setup_shell_env(config.get('shell_env', {}))
+
+    work_dir = config.get('work_dir', project)
+    work_dir = loader.expand_path(work_dir)
+
+    python_bin = loader.get_python_bin()
+    # bugfix, command like `odoo.py shell`, the word 'shell'  must be mentioned
+    # before we define --config, weird
+    binargs = [python_bin, __file__]
+    binargs.append('shell')
+    binargs.append('--no-xmlrpc')
+    if config_file:
+        binargs.append('--config=' + config_file)
+
+    os.chdir(work_dir)
+
+    environ = os.environ.copy()
+    environ['IPY_TEST_SIMPLE_PROMPT'] = '1'
+    prc = pexpect.spawn(' '.join(binargs), echo=False, env=environ,
+            logfile=sys.stdout)
+
+    prc.expect(r'In \[\d+\]:')
+    prc.sendline("IrModule = env['ir.module.module']")
+    for mod in args:
+        prc.expect(r'In \[\d+\]:')
+        prc.sendline("mod = IrModule.search([('name', '=', '%s')]).exists()"\
+                % mod)
+
+        prc.expect(r'In \[\d+\]:')
+        prc.sendline("if not mod: IrModule.update_list()")
+
+        prc.expect(r'In \[\d+\]:')
+        prc.sendline("if not mod: mod = IrModule.search([('name', '=', " +\
+                "'%s')]).exists()" % mod)
+
+        prc.expect(r'In \[\d+\]:')
+        prc.sendline("if mod and mod.state not in ('installed', " +\
+                "'to upgrade'): mod.button_immediate_install()")
+
+    prc.expect(r'In \[\d+\]:')
+    prc.sendline('quit')
+
+
+def odoo_uninstall(loader, project=None, variant=None, *args):
+    loader.setup_virtualenv()
+
+    project, variant = loader.setup_project_env(project, variant)
+
+    config = loader.config.get('configuration', {})
+    config = config.get(variant, {})
+    config = config.get(project, {})
+
+    config_file = config.get('config_file')
+    config_file = os.path.join(loader.config['work_dir'],
+            config_file)
+
+    loader.setup_shell_env(config.get('shell_env', {}))
+
+    work_dir = config.get('work_dir', project)
+    work_dir = loader.expand_path(work_dir)
+
+    python_bin = loader.get_python_bin()
+    # bugfix, command like `odoo.py shell`, the word 'shell'  must be mentioned
+    # before we define --config, weird
+    binargs = [python_bin, __file__]
+    binargs.append('shell')
+    binargs.append('--no-xmlrpc')
+    if config_file:
+        binargs.append('--config=' + config_file)
+
+    os.chdir(work_dir)
+
+    environ = os.environ.copy()
+    environ['IPY_TEST_SIMPLE_PROMPT'] = '1'
+    prc = pexpect.spawn(' '.join(binargs), echo=False, env=environ,
+            logfile=sys.stdout)
+
+    prc.expect(r'In \[\d+\]:')
+    prc.sendline("IrModule = env['ir.module.module']")
+    for mod in args:
+        prc.expect(r'In \[\d+\]:')
+        prc.sendline("mod = IrModule.search([('name', '=', '%s')]).exists()"\
+                % mod)
+
+        prc.expect(r'In \[\d+\]:')
+        prc.sendline("if not mod: IrModule.update_list()")
+
+        prc.expect(r'In \[\d+\]:')
+        prc.sendline("if not mod: mod = IrModule.search([('name', '=', " +\
+                "'%s')]).exists()" % mod)
+
+        prc.expect(r'In \[\d+\]:')
+        prc.sendline("if mod.state != 'uninstalled': " +\
+                "mod.button_immediate_uninstall()")
+
+    prc.expect(r'In \[\d+\]:')
+    prc.sendline('quit')
+
+
+def odoo_upgrade(loader, project=None, variant=None, *args):
+    loader.setup_virtualenv()
+
+    project, variant = loader.setup_project_env(project, variant)
+
+    config = loader.config.get('configuration', {})
+    config = config.get(variant, {})
+    config = config.get(project, {})
+
+    config_file = config.get('config_file')
+    config_file = os.path.join(loader.config['work_dir'],
+            config_file)
+
+    loader.setup_shell_env(config.get('shell_env', {}))
+
+    work_dir = config.get('work_dir', project)
+    work_dir = loader.expand_path(work_dir)
+
+    python_bin = loader.get_python_bin()
+    # bugfix, command like `odoo.py shell`, the word 'shell'  must be mentioned
+    # before we define --config, weird
+    binargs = [python_bin, __file__]
+    binargs.append('shell')
+    binargs.append('--no-xmlrpc')
+    if config_file:
+        binargs.append('--config=' + config_file)
+
+    os.chdir(work_dir)
+
+    environ = os.environ.copy()
+    environ['IPY_TEST_SIMPLE_PROMPT'] = '1'
+    prc = pexpect.spawn(' '.join(binargs), echo=False, env=environ,
+            logfile=sys.stdout)
+
+    prc.expect(r'In \[\d+\]:')
+    prc.sendline("IrModule = env['ir.module.module']")
+    for mod in args:
+        prc.expect(r'In \[\d+\]:')
+        prc.sendline("mod = IrModule.search([('name', '=', '%s')]).exists()"\
+                % mod)
+
+        prc.expect(r'In \[\d+\]:')
+        prc.sendline("if not mod: IrModule.update_list()")
+
+        prc.expect(r'In \[\d+\]:')
+        prc.sendline("if not mod: mod = IrModule.search([('name', '=', " +\
+                "'%s')]).exists()" % mod)
+
+        prc.expect(r'In \[\d+\]:')
+        prc.sendline("if mod and mod.state not in ('installed', " +\
+                "'to upgrade'): mod.button_immediate_install()")
+
+        prc.expect(r'In \[\d+\]:')
+        prc.sendline("if mod and mod.state == 'installed': " +\
+                "mod.button_immediate_upgrade()")
+
+    prc.expect(r'In \[\d+\]:')
+    prc.sendline('quit')
+
 def odoo_setup(loader, project=None, variant=None, *args):
     loader.setup_virtualenv()
     python_bin = loader.get_python_bin()
@@ -47,7 +252,8 @@ def odoo_setup(loader, project=None, variant=None, *args):
     os.execvp(binargs[0], binargs)
 
 
-commands = (odoo, odoo_setup)
+commands = (odoo, odoo_setup, odoo_shell, odoo_install, odoo_uninstall,
+        odoo_upgrade)
 
 
 def main():
