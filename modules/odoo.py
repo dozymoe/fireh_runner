c7391ff6493066e882e08d5fa6366ccf228c5553
Fahri Reza <dozymoe@gmail.com>
2018-12-05 18:54:21 +0700

    odoo-shell to upgrade module is buggy
    Sometimes errors about unintialized model fields.

--- modules/odoo.py
+++ modules/odoo.py
@@ -5,6 +5,7 @@ Odoo is an OpenERP framework.
 Website: http://www.odoo.com
 """
 import os
+import subprocess
 import sys
 from time import sleep
 
@@ -225,7 +226,6 @@ def odoo_uninstall(loader, project=None, variant=None, *args):
 
 def odoo_upgrade(loader, project=None, variant=None, *args):
     loader.setup_virtualenv()
-    import pexpect
 
     project, variant = loader.setup_project_env(project, variant)
 
@@ -246,60 +246,18 @@ def odoo_upgrade(loader, project=None, variant=None, *args):
     # bugfix, command like `odoo.py shell`, the word 'shell'  must be mentioned
     # before we define --config, weird
     binargs = [python_bin, __file__]
-    binargs.append('shell')
     binargs.append('--no-xmlrpc')
+    binargs.append('--stop-after-init')
     if config_file:
         binargs.append('--config=' + config_file)
 
     os.chdir(work_dir)
-
-    environ = os.environ.copy()
-    environ['IPY_TEST_SIMPLE_PROMPT'] = '1'
-    spawn_args = {
-        'echo': False,
-        'env': environ,
-        'logfile': sys.stdout,
-        'timeout': SHELL_TIMEOUT,
-    }
-    if sys.version_info[0] > 2:
-        spawn_args['encoding'] = 'utf-8'
-    prc = pexpect.spawn(' '.join(binargs), **spawn_args)
-    prc.expect(r'In \[\d+\]:')
-    prc.sendline("IrModule = env['ir.module.module']")
     for mod in args:
-        prc.expect(r'In \[\d+\]:')
-        prc.sendline("mod = IrModule.search([('name', '=', '%s')]).exists()"\
-                % mod)
+        ret = subprocess.call(binargs + ['-u', mod])
+        if ret:
+            exit(ret)
+    exit(0)
 
-        prc.expect(r'In \[\d+\]:')
-        prc.sendline("if not mod: IrModule.update_list()")
-
-        prc.expect(r'In \[\d+\]:')
-        prc.sendline("if not mod: mod = IrModule.search([('name', '=', " +\
-                "'%s')]).exists()" % mod)
-
-        prc.expect(r'In \[\d+\]:')
-        prc.sendline("if mod and mod.state not in ('installed', " +\
-                "'to upgrade'): mod.button_immediate_install()")
-
-        prc.expect(r'In \[\d+\]:')
-        prc.sendline("if not mod:")
-        prc.expect(r'\s+\.+:')
-        prc.sendline("    print('Module \"%s\" not found!')" % mod)
-        prc.expect(r'\s+\.+:')
-        prc.sendline("    quit()")
-        prc.expect(r'\s+\.+:')
-        prc.sendline("")
-
-        exp = prc.expect([pexpect.EOF, r'In \[\d+\]:'])
-        if exp == 0:
-            break
-        else:
-            prc.sendline("if mod and mod.state == 'installed': " +\
-                    "mod.button_immediate_upgrade()")
-
-    prc.expect(r'In \[\d+\]:')
-    prc.sendline('quit')
 
 def odoo_setup(loader, project=None, variant=None, *args):
     venv_type = loader.setup_virtualenv()
