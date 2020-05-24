47faed2115a1bbab593c6a280aab9e0083a6c6f1
Fahri Reza <dozymoe@gmail.com>
2020-05-25 00:13:40 +0700

    deprecate pip runner

--- modules/python.py
+++ modules/python.py
@@ -2,7 +2,32 @@
 """
 import os
 
+def pip(loader, *args):
+    """Install or uninstall python packages."""
+    venv_type = loader.setup_virtualenv()
+    python_bin = loader.get_python_bin()
+
+    pip_args = list(args)
+    for arg in args:
+        if arg.startswith('-'):
+            continue
+        if arg == 'install':
+            if venv_type == 'python':
+                pip_args.append('--user')
+            pip_args.extend(loader.config.get('pip_install_args', []))
+
+        elif arg == 'freeze':
+            if venv_type == 'python':
+                pip_args.append('--user')
+        else:
+            break
+
+    binargs = [python_bin, '-m', 'pip'] + pip_args
+    os.execvp(binargs[0], binargs)
+
+
 def python(loader, project=None, variant=None, *args): #pylint:disable=keyword-arg-before-vararg
+    """Python shell."""
     project, variant = loader.setup_project_env(project, variant)
     loader.setup_virtualenv()
     loader.setup_shell_env()
@@ -12,4 +37,4 @@ def python(loader, project=None, variant=None, *args): #pylint:disable=keyword-a
     os.execvp(binargs[0], binargs)
 
 
-commands = (python,)
+commands = (pip, python)
