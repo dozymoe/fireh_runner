6d03b2d48b4ab60979ecbf8e1804560ad10894ef
Fahri Reza <dozymoe@gmail.com>
2020-11-03 04:44:45 +0700

    change dir if --project was set

--- modules/python.py
+++ modules/python.py
@@ -28,10 +28,17 @@ def pip(loader, *args):
 
 def python(loader, project=None, variant=None, *args): #pylint:disable=keyword-arg-before-vararg
     """Python shell."""
+    project_selected = project is not None
     project, variant = loader.setup_project_env(project, variant)
     loader.setup_virtualenv()
     loader.setup_shell_env()
     python_bin = loader.get_python_bin()
+
+    if project_selected:
+        config = loader.get_project_config()
+        work_dir = config.get('work_dir', project)
+        work_dir = loader.expand_path(work_dir)
+        os.chdir(work_dir)
 
     binargs = [python_bin] + list(args)
     os.execvp(binargs[0], binargs)
