4abb804535ea6d68b00fa19ddcf5b58f5a73f281
Fahri Reza <dozymoe@gmail.com>
2021-11-02 00:37:25 +0700

    added yarn setup modules

---
+++ setup_modules/yarn.py
@@ -0,0 +1,27 @@
+import os
+from shutil import copyfile, rmtree
+from subprocess import check_call
+
+def setup(loader, variant=None):
+    _, variant = loader.setup_project_env(None, variant)
+    work_dir = loader.config['work_dir']
+
+    use_symlink = not loader.config.get('no_symlink_please', False)
+    if use_symlink:
+        link_fn = loader.force_symlink
+    else:
+        link_fn = copyfile
+
+    package_path = os.path.join(work_dir, 'package.json')
+    package_var_path = os.path.join(work_dir, 'package-%s.json' % variant)
+    if os.path.exists(package_var_path):
+        link_fn(package_var_path, package_path)
+
+    if os.path.exists(package_path):
+        print("Setup node_modules")
+
+        mod_dir = os.path.join(work_dir, 'node_modules')
+        if os.path.exists(mod_dir):
+            rmtree(mod_dir, ignore_errors=True)
+
+        check_call(['yarn', '--verbose', 'install'], cwd=work_dir)
