b320693fbdb605a756aae87f7b3e56ef75baf228
Fahri Reza <dozymoe@gmail.com>
2023-01-14 14:25:00 +0700

    fallback to uwsgi installed in system

--- modules/uwsgi.py
+++ modules/uwsgi.py
@@ -5,12 +5,21 @@ Website: https://uwsgi-docs.readthedocs.io/en/latest/
 import os
 import shutil
 import subprocess
-import sys
+
+def _find_uwsgi_bin(loader):
+    venv_dir = loader.get_virtualenv_dir()
+    uwsgi_bin = os.path.join(venv_dir, 'bin', 'uwsgi')
+    if os.path.exists(uwsgi_bin):
+        return [uwsgi_bin]
+    uwsgi_bin = shutil.which('uwsgi')
+    if os.path.exists(uwsgi_bin):
+        return [uwsgi_bin, '--plugin=python3']
+    raise Exception("Cannot find uwsgi binary.\n")
+
 
 def uwsgi(loader, *args):
     loader.setup_virtualenv()
-    venv_dir = loader.get_virtualenv_dir()
-    binargs = [os.path.join(venv_dir, 'bin', 'uwsgi')] + list(args)
+    binargs = _find_uwsgi_bin(loader) + list(args)
     os.execvp(binargs[0], binargs)
 
 
@@ -20,9 +29,7 @@ def uwsgi_run(loader, project=None, variant=None, *args): #pylint:disable=keywor
     loader.setup_shell_env()
     config = loader.get_project_config()
 
-    venv_dir = loader.get_virtualenv_dir()
-    binargs = [os.path.join(venv_dir, 'bin', 'uwsgi'), '--master',
-            '--die-on-term']
+    binargs = _find_uwsgi_bin(loader) + ['--master', '--die-on-term']
     if not loader.is_production():
         binargs.append('--honour-stdin')
 
@@ -36,16 +43,15 @@ def uwsgi_run(loader, project=None, variant=None, *args): #pylint:disable=keywor
 
 def uwsgi_build_cheaper2(loader):
     url = 'https://github.com/KLab/uwsgi-cheaper-spare2'
-    exe = sys.argv[0]
-    workdir = os.path.dirname(exe)
-    subprocess.check_call([exe, 'uwsgi', f'--build-plugin={url}'])
+    loader.setup_virtualenv()
+    binargs = _find_uwsgi_bin(loader) + [f'--build-plugin={url}']
+
+    subprocess.check_call(binargs)
     # cleanup
-    tmpdir = os.path.join(workdir, '.uwsgi_plugins_builder')
-    if os.path.exists(tmpdir):
-        shutil.rmtree(tmpdir)
-    tmpdir = os.path.join(workdir, 'uwsgi-cheaper-spare2')
-    if os.path.exists(tmpdir):
-        shutil.rmtree(tmpdir)
+    if os.path.exists('.uwsgi_plugins_builder'):
+        shutil.rmtree('.uwsgi_plugins_builder')
+    if os.path.exists('uwsgi-cheaper-spare2'):
+        shutil.rmtree('uwsgi-cheaper-spare2')
 
 
 commands = (uwsgi, uwsgi_run, uwsgi_build_cheaper2)
